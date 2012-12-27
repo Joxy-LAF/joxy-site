2433b712955751d11390cbd14771e57c60347bbe
Thom Castermans <thomcastermans@users.sourceforge.net>
2012-12-27 22:29:40 +0100

    Fixed links and updated answers.

--- faq.php
+++ faq.php
@@ -16,20 +16,20 @@ The <em>J</em> is for Java, and <em>oxy</em> is for Oxygen.</p>
 <p><strong>What is that icon for Joxy?</strong><br />
 It is Duke, Java's mascot, sitting on Konqi, KDE's mascot. We made the image by combining 3D models of Duke and Konqi. See the <a href="<?= APP_PREFIX ?>/about/#logo">about page</a>.</p>
 <p><strong>How do I get Joxy?</strong><br />
-You can either download it, or compile it yourself. You can download Joxy with the <em>Downloads</em> button above. If you want to compile Joxy yourself, please see <a class="alink" href="/p/joxy/wiki/Downloading%20the%20source/">[Downloading the source]</a> and <a class="alink" href="/p/joxy/wiki/Compiling%20the%20source/">[Compiling the source]</a>.</p>
+You can either download it, or compile it yourself. You can download Joxy <a href="<?= APP_PREFIX ?>/download/">here</a>. If you want to compile Joxy yourself, please read <a href="<?= APP_PREFIX ?>/documentation/download">Downloading Joxy</a> and <a href="<?= APP_PREFIX ?>/documentation/compile">Compiling Joxy</a> in the documentation.</p>
 <p><strong>How do I put Joxy on a program?</strong><br />
-We composed a page with instructions about that here: <a class="alink" href="/p/joxy/wiki/Using%20Joxy/">[Using Joxy]</a>.</p>
+We composed a page with instructions about that here: <a href="<?= APP_PREFIX ?>/documentation/usage">Applying Joxy to an application</a>.</p>
 <p><strong>Is Joxy finished yet?</strong><br />
 No, surely not! The current version is far from being complete and contains bugs. Hence the version number 0.1.0.</p>
 <p><strong>How does the version numbering scheme work?</strong><br />
 The current version is 0.1.0. The first digit is a 0, because Joxy is not stable yet. We just increment the last digit for every new version and if there is a large bugfix release, we increment the middle digit. So the versions with a 0 in the end are meant to be more stable, but in this early development phase we still recommend using the latest version.</p>
 <p><strong>When will the next version of Joxy be released?</strong><br />
-The following Joxy version will probably be 0.1.1, containing new features.</p>
+The following Joxy version will probably be 0.1.1, containing new features. We are not sure when this version will be released.</p>
 <p><strong>How can I help with improving Joxy?</strong><br />
-The best way is probably by testing applications with Joxy, and reporting bugs.</p>
+The best way is probably by testing applications with Joxy, and reporting bugs. If you want to help develop Joxy, please leave a message in the forum, or drop us a line using the SourceForge message facility. You will be more than welcome :-)</p>
 <p><strong>What systems are supported?</strong><br />
-Firstly, only KDE systems on Linux are supported at the moment.<br />
-We only test Joxy on two systems (Debian Testing and Ubuntu 12.04), but it should work with others. If you find problems please report them. Also, if you use another distribution and the installation/compilation instructions are incorrect for this distribution, please report that too, so we can change them.</p>
+Officially, only KDE systems on Linux are supported at the moment.<br />
+We only test Joxy on two systems (Debian Testing and Ubuntu 12.04), but it should work with other distributions too. If you find problems please report them. Also, if you use another distribution and the installation/compilation instructions are incorrect for this distribution, please report that too, so we can change them.</p>
 <p><strong>Will other operating systems be supported in the future?</strong><br />
 The current Git version should run on other platforms (Windows/Mac/Linux without KDE) too, but we have not tested that very thoroughly. The most important problem is the absence of the Oxygen icons.</p>
 <p><strong>How are widgets rendered?</strong><br />
